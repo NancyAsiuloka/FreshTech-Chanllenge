94122355baa12117d02f045ef6c28904a221be53
Nancy Asiuloka <nancyasiuloka@gmail.com>
2024-08-30 19:10:18 +0100

    added some changes

--- controllers/error.js
+++ controllers/error.js
@@ -1,54 +1,69 @@
-const AppError = require('../utils/appError')
+const AppError = require('../utils/appError');
 
 const handleCastErrorDB = err => {
     const message = `Invalid ${err.path}: ${err.value}`;
-    return new AppError(message, 400)
-}
+    return new AppError(message, 400);
+};
 
 const handleDuplicateFieldsDB = err => {
     const value = err.message.match(/(["'])(?:\\.|[^\\])*?\1/)[0];
-    console.log(value)
-    const message = `Duplicate field value: $[value], please use another value`;
-    return new AppError(message, 400 )
-}
+    const message = `Duplicate field value: ${value}, please use another value!`;
+    return new AppError(message, 400);
+};
 
 const handleValidationErrorDB = err => {
-    const errors = Object.values(err.errors).map(el=> el.message);
-
-
+    const errors = Object.values(err.errors).map(el => el.message);
     const message = `Invalid input data: ${errors.join('. ')}`;
-    return new AppError(message, 400)
-}
+    return new AppError(message, 400);
+};
 
-const handleJWTError = () => new AppError('Invalid token. Please login again!', 401)
-const handleJWTExpiredError = () => new AppError('Your token has expired. Please login again', 401)
+const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () => new AppError('Your token has expired. Please log in again!', 401);
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
-        status: 'fail',
+        status: err.status,
         error: err,
         message: err.message,
-        stack: err.stack
-    })
-}
+        stack: err.stack,
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        console.error('ERROR 💥', err);
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!',
+        });
+    }
+};
 
 module.exports = (err, req, res, next) => {
-
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    if(process.env.NODE_ENV === 'development'){
+    if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
-    } else if(process.env.NODE_ENV === 'production'){
+    } else if (process.env.NODE_ENV === 'production') {
         let error = {...err};
+        error.message = err.message;
+        error.name = err.name;
 
-        if(error.name === "CastError") error = handleCastErrorDB(error)
-        if(error.code === 11000) error = handleDuplicateFieldsDB(error)
-        if(error.name === "validationError") error = handleValidationErrorDB(error)
-        if(error.name === "jsonWebTokenError") error = handleJWTError()
-        if(error.name === "TokenExpiredError") error = handleJWTExpiredError()
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+        if (error.name === 'JsonWebTokenError') error = handleJWTError();
+        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-        sendErrorProd(error, res)
+        sendErrorProd(error, res);
     }
-    next()
+    next();
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require("http");
 const { dbConnect } = require("./config.js/db");
 const bodyParser = require("body-parser")
 const userRouter = require('./routes/userRoute');
+const AppError = require('./utils/appError')
 
 const app = express();
 const server = http.createServer(app);
@@ -16,6 +17,14 @@ app.use(bodyParser.json());
 
 
 app.use('/api/v1/users', userRouter);
+
+// Handling 404 Errors
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+})
+
+// Global error handler
+app.use(require('./controllers/error'));
 
 const port = process.env.PORT;
 server.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
